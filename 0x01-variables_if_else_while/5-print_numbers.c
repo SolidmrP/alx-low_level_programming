c445d53777d0d94cf451a794e66142cf37347e3c
SolidmrP <praiseworks90@gmail.com>
2022-09-11 14:58:46 -0700

    numbers in base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print all single numbers
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 10)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
