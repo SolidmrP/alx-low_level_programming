b562d92e06073fd857ef7d87769718e901dc41e4
SolidmrP <praiseworks90@gmail.com>
2022-09-13 15:29:17 -0700

    add two int

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - add two int
+ * @num1: first character to test
+ * @num2: second character
+ * Return: result
+ */
+int add(int num1, int num2)
+{
+	int res;
+
+	res = num1 + num2
+		return (res);
+}
