ea2a248fa619ce5bc2485d83ed3f645cf521116d
SolidmrP <praiseworks90@gmail.com>
2022-11-07 04:51:16 -0800

    quick fix

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -24,11 +24,11 @@ int main(int argc, char *argv[])
 		if (cents >= 25)
 			cents -= 25;
 		else if (cents >= 10)
-			cemts -= 10;
+			cents -= 10;
 		else if (cents >= 5)
 			cents -= 5;
 		else if (cents >= 2)
-			cemts -= 2;
+			cents -= 2;
 		else if (cents >= 1)
 			cents -= 1;
 		ncoins += 1;
