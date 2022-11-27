0160dbbc3db507dc9c8e9fd9b84c9cc1be21acd9
SolidmrP <praiseworks90@gmail.com>
2022-11-27 10:04:24 -0800

    hot fix

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,7 +1,9 @@
-#ifndef _main_h_
-#define _main_h_
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
+
+#endif
